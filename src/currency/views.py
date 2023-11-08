b7707f12deacc1e671df442e01aadfc26ef7d7d0
GPU <gpungeot+dev@gmail.com>
2023-11-08 11:46:02 +0100

    Several lines on graph

--- src/currency/views.py
+++ src/currency/views.py
@@ -5,4 +5,4 @@ from currency import api
 # Create your views here.
 def dashboard(request, days_range = 2, currencies="USD"):
     days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
-    return render(request, "currency/index.html", context={"data": rates["USD"], "days_labels": days})
+    return render(request, "currency/index.html", context={"data": rates, "days_labels": days})
